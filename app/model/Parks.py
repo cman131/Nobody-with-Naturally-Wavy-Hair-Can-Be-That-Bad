703cc3776b0765e9446298f13c7e405a34e9de35
Wright <cwright@constantcontact.com>
2015-07-23 09:09:19 -0400

    added configuration files

--- app/model/Parks.py
+++ app/model/Parks.py
@@ -1,3 +1,23 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://' + app.config['MYSQL_DATABASE_USER'] + ':' + app.config['MYSQL_DATABASE_PASSWORD'] + '@' + app.config['MYSQL_DATABASE_HOST']
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(45))
+    size = db.Column(db.String(45))
+    address = db.Column(db.String(45))
+    zip_code = db.Column(db.String(45))
+    state = db.Column(db.String(45))
+    city = db.Column(db.String(45))
+    playground = db.Column(db.Integer)
+
+
+
+"""
 from flask.ext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
 
@@ -19,3 +39,4 @@ if len(data) is 0:
     return json.dumps({'message':'User created successfully !'})
 else:
     return json.dumps({'error':str(data[0])})
+"""
