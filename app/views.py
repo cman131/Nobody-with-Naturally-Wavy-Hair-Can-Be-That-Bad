acc4f60e4becc4a8f63c156a8a9d4c3a704625bc
Conor Wright <conor13@msn.com>
2015-08-01 23:15:09 -0400

    began work on the gps distance checker

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+from utils import filterToLocation
 
 @app.route('/')
 @app.route('/index')
@@ -9,6 +10,9 @@ def index():
 
 @app.route('/results')
 def results():
+	distance = request.args.get('dist') ? : 10
+	coordinates = (request.args.get('lat'), request.args.get('long'))
 	user = {'name': 'Conor'}
-	results = [{'name':'Result 1'}, {'name':'Result 2'}, {'name':'Result 3'}]
+	results = [{'name':'Result 1', 'latitude': -70.909343, 'longitude': 34.76876}, {'name':'Result 2', 'latitude': 39.67, 'longitude': 75.61}, {'name':'Result 3', 'latitude': 42.37, 'longitude': -71.25}]
+	results = filterToLocation(results, coordinates, distance)
 	return render_template('results.html', title='Home', user=user, results=results)
