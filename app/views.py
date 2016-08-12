ab9e3e825f1d1d99a0219d6d669c743b365720ae
Conor Wright <conor13@msn.com>
2016-08-11 23:57:21 -0400

    proper ternary

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template
-from utils import filterToLocation
+import utils
 
 @app.route('/')
 @app.route('/index')
@@ -10,7 +10,7 @@ def index():
 
 @app.route('/results')
 def results():
-	distance = request.args.get('dist') ? : 10
+	distance = request.args.get('dist') if request.args.get('dist') != None else 10
 	coordinates = (request.args.get('lat'), request.args.get('long'))
 	user = {'name': 'Conor'}
 	results = [{'name':'Result 1', 'latitude': -70.909343, 'longitude': 34.76876}, {'name':'Result 2', 'latitude': 39.67, 'longitude': 75.61}, {'name':'Result 3', 'latitude': 42.37, 'longitude': -71.25}]
